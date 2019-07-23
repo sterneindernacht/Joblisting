4e6cd4ed58238ae3e1894cfeae4d25cc8720f172
sterneindernacht <kaczmarek.anita0@gmail.com>
2019-07-23 14:36:02 +0200

    ...

--- src/main/java/sterneindernacht/com/github/joblisting/authorization/service/SecurityService.java
+++ src/main/java/sterneindernacht/com/github/joblisting/authorization/service/SecurityService.java
@@ -3,5 +3,5 @@ package sterneindernacht.com.github.joblisting.authorization.service;
 public interface SecurityService {
     String findLoggedInUsername();
 
-    void autoLogin(String username, String password);
+      void autoLogin(String username, String password);
 }
